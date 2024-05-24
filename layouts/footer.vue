46b7532ec4cb2b8f59ef1e92f4084af5ced6ee23
Andres <andresdreamerf@gmail.com>
2024-05-24 17:29:02 -0400

    footer mobile

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -1,21 +1,23 @@
 <template>
-    <footer class="py-8 bg-white hidden lg:block">
-        <div class="w-[730px] h-full mx-auto grid grid-cols-3 gap-[54px]">
-            <div>
+    <div class="border-t border-gray-300"></div>
+    <footer class="px-4 lg:px-0 py-2 lg:py-8 bg-white">
+        <img class="w-[228px] h-[88px] mx-auto lg:hidden"src="/assets/fulllogoslogan.svg" alt="fulllogo particulistas"/>
+        <div class="lg:w-[730px] h-full flex justify-between lg:mx-auto lg:grid lg:grid-cols-3 lg:gap-[54px]">
+            <div class="hidden lg:inline-block">
                 <img src="/assets/fulllogoslogan.svg" alt="fulllogo particulistas"/>
             </div>
             <div class="px-2.5">
                 <h1 class="text-[24px] font-bold my-2 color-4A4A4A">Institucional</h1>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">¿Quienes somos?</button>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Filosofía particulista</button>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Colabora con particulista</button>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Contacto</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">¿Quienes somos?</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Filosofía particulista</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Colabora con particulista</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Contacto</button>
             </div>
             <div class="px-2.5">
-                <h1 class="text-[24px] font-bold my-2 color-4A4A4A">Legal</h1>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Condiciones de uso</button>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Protección de datos</button>
-                <button class="text-sm font-bold my-2 color-27ABB1 hover-E786F0">Política de cookies</button>
+                <h1 class="text-[24px] text-left block font-bold my-2 color-4A4A4A">Legal</h1>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Condiciones de uso</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Protección de datos</button>
+                <button class="text-sm text-left block font-bold my-2 color-27ABB1 hover-E786F0">Política de cookies</button>
             </div>
         </div>
     </footer>
